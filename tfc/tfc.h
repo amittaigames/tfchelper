71d2e483ec0fa7c9304d9b66d9cbeb7982022bdc
Gandalf1209 (Jonah) <amittai@techpandainc.com>
2017-01-30 18:35:27 +0000

    [list_dev] Commit #0002

--- tfc/tfc.h
+++ tfc/tfc.h
@@ -1,6 +1,11 @@
 #ifndef TFC_H
 #define TFC_H
 
+#ifdef TFC_BOOL
+    #define TRUE 1
+    #define FALSE 0
+#endif
+
 #include "tfc_list.h"
 #include "tfc_string.h"
 
--- tfc/tfc_list.h
+++ tfc/tfc_list.h
@@ -3,19 +3,53 @@
 
 #include <stdlib.h>
 
-typedef struct _List {
-    void* data;
-    int data_size;
+typedef struct _IntList {
+    int* data;
     int ptr;
     int size;
-} List;
+} IntList;
 
-List*       List_new(int data_size, int size);
+IntList*        IntList_new(int size);
+void            IntList_set(IntList* self, int index, int data);
+void            IntList_add(IntList* self, int data);
+int             IntList_get(IntList* self, int index);
+void            IntList_delete(IntList* self);
 
 //
 //  IMPLEMENTATION
 //
 
+IntList* IntList_new(int size) {
+    IntList* self = (IntList*) malloc(sizeof(IntList));
+    
+    self->data = (int*) malloc(sizeof(int) * size);
+    self->ptr = 0;
+    self->size = size;
+    
+    return self;
+}
 
+void IntList_set(IntList* self, int index, int data) {
+    if (index >= self->size) {
+        self->size *= 2;
+        self->data = (int*) realloc(self->data, sizeof(int) * self->size);
+        IntList_set(self, index, data);
+    }
+    
+    self->data[index] = data;
+}
+
+void IntList_add(IntList* self, int data) {
+    IntList_set(self, self->ptr++, data);
+}
+
+int IntList_get(IntList* self, int index) {
+    return self->data[index];
+}
+
+void IntList_delete(IntList* self) {
+    free(self->data);
+    free(self);
+}
 
 #endif // TFC_LIST_H
